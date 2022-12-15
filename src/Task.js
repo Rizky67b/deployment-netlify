d4034fd35c69cfac18cda2becf03483eccddbbd1
Rizky67b <rizki67b@gmail.com>
2022-12-15 21:04:19 +0700

    commit ketiga

--- src/Task.js
+++ src/Task.js
@@ -1,4 +1,4 @@
-const url = "YOURDEPLOYURL" // TODO: replace this
+const url = "https://poetic-genie-55b41b.netlify.app/" // TODO: replace this
 const studentName = "Juniawan Rizky Firdaus" // TODO: replace this
 const studentId = "FE3191622" // TODO: replace this
 
