a077092c29d7917415eccafcdd2eee21af276ec5
Rizky67b <rizki67b@gmail.com>
2022-12-15 20:20:05 +0700

    first commit

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Juniawan Rizky Firdaus" // TODO: replace this
+const studentId = "FE3191622" // TODO: replace this
+
+export { url, studentName, studentId }
